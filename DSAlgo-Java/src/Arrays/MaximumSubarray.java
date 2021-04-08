504eeefe587460320d0f5457e09e051c9d405fcc
NehaChopra <nchopra456@gmail.com>
2021-04-08 17:05:05 +0530

    Day4

--- DSAlgo-Java/src/Arrays/MaximumSubarray.java
+++ DSAlgo-Java/src/Arrays/MaximumSubarray.java
@@ -1,48 +1,49 @@
 /*
- * https://leetcode.com/problems/maximum-subarray/
+ * https://leetcode.com/problems/maximum-product-subarray/
  * 
- * Example 1:
-
-	Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+	Example 1:
+	
+	Input: nums = [2,3,-2,4]
 	Output: 6
-	Explanation: [4,-1,2,1] has the largest sum = 6.
+	Explanation: [2,3] has the largest product 6.
 	Example 2:
 	
-	Input: nums = [1]
-	Output: 1
-	Example 3:
-	
-	Input: nums = [5,4,-1,7,8]
-	Output: 23
-
- 
+	Input: nums = [-2,0,-1]
+	Output: 0
+	Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
  */
 package Arrays;
 
 public class MaximumSubarray {
 	public static void main(String[] args) {
-//		int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-		//
-//		 int[] nums = {1};
-		//
-		 int[] nums = {5,4,-1,7,8};
-
+//		 int[] nums = { 2,3,-2,4 };
+//		 int[] nums = {-2,0,-1};
+//		 int[] nums = {-3,-1,-1};
+//		 int[] nums = {0,2};
+//		 int[] nums = {0};
+//		int[] nums = { 3, -1, 4 };
+		int[] nums = { -2, 3, -4};
+//
 		System.out.println(maxSubarray(nums));
 	}
 
-	// Kandane Algorithm
 	public static int maxSubarray(int[] nums) {
-		int maxSum = 0;
-		int sum = 0;
-		for (int index = 0; index < nums.length; index++) {
-			sum = sum + nums[index];
-			if (sum < 0) {
-				sum = 0;
+		int max = nums[0];
+		int maxProd = max;
+		int minProd = max;
+
+		for (int index = 1; index < nums.length; index++) {
+			// swap because neg*neg becoms positive
+			if (nums[index] < 0) {
+				int temp = maxProd;
+				maxProd = minProd;
+				minProd = temp;
 			}
-			if (sum > maxSum) {
-				maxSum = sum;
-			}
+			maxProd = Math.max(nums[index], maxProd * nums[index]);
+			minProd = Math.min(nums[index], minProd * nums[index]);
+			
+			max = Math.max(max, maxProd);
 		}
-		return maxSum;
+		return max;
 	}
 }
--- DSAlgo-Java/src/Arrays/SubarraySumEqualsK.java
+++ DSAlgo-Java/src/Arrays/SubarraySumEqualsK.java
@@ -21,8 +21,50 @@ public class SubarraySumEqualsK {
 		int k = 3;
 		System.out.println(countSubarrySumEqualK(nums, k));
 	}
+	
+	public static void heapSort(int[] inputArray) {
+		for (int index = inputArray.length / 2 - 1; index >= 0; index--) {
+			heapify(index, inputArray.length, inputArray);
+		}
+
+		for (int index = inputArray.length - 1; index >= 0; index--) {
+			swap(0, index, inputArray);
+			heapify(0, index, inputArray);
+		}
+	}
+
+	public static void heapify(int index, int end, int[] inputArray) {
+		int largest = index;
+		int left = leftChild(index);
+		int right = rightChild(index);
+		if (left < end && inputArray[left] > inputArray[largest]) {
+			largest = left;
+		}
+		if (right < end && inputArray[right] > inputArray[largest]) {
+			largest = right;
+		}
+		if (largest != index) {
+			swap(index, largest, inputArray);
+			heapify(largest, end, inputArray);
+		}
+	}
+
+	public static int leftChild(int index) {
+		return ((2 * index) + 1);
+	}
+
+	public static int rightChild(int index) {
+		return ((2 * index) + 2);
+	}
+
+	public static void swap(int iIndex, int jIndex, int[] inputArray) {
+		int temp = inputArray[iIndex];
+		inputArray[iIndex] = inputArray[jIndex];
+		inputArray[jIndex] = temp;
+	}
 
 	public static int countSubarrySumEqualK(int []nums, int k){
+		heapSort(nums);
 		int sum = 0;
 		int count = 0;
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
