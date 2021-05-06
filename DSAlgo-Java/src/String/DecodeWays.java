2fab63351b820390f35d32bc8862329c21dbc2b7
NehaChopra <nchopra456@gmail.com>
2021-05-06 17:10:58 +0530

    Day 1

---
+++ DSAlgo-Java/src/String/DecodeWays.java
@@ -0,0 +1,79 @@
+/*
+ * https://leetcode.com/problems/decode-ways/
+ * A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+"AAJF" with the grouping (1 1 10 6)
+"KJF" with the grouping (11 10 6)
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+Given a string s containing only digits, return the number of ways to decode it.
+
+The answer is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+Example 3:
+
+Input: s = "0"
+Output: 0
+Explanation: There is no character that is mapped to a number starting with 0.
+The only valid mappings with 0 are 'J' -> "10" and 'T' -> "20", neither of which start with 0.
+Hence, there are no valid ways to decode this since all digits need to be mapped.
+Example 4:
+
+Input: s = "06"
+Output: 0
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+ */
+package String;
+
+public class DecodeWays {
+	public static int decodeWays(String str) {
+		int count = 0;
+		if (str == null || str.length() == 0) {
+			return count;
+		}
+		int strLength = str.length();
+		int array[] = new int[strLength + 1];
+		array[0] = 1;
+		array[1] = str.charAt(0) == '0' ? 0 : 1;
+		for (int index = 2; index <= strLength; index++) {
+			int oneCharElement = Integer.valueOf(str.substring(index - 1, index));
+			int twoCharElement = Integer.valueOf(str.substring(index - 2, index));
+			if (oneCharElement >=1 && oneCharElement <= 9) {
+				array[index] = array[index] + array[index - 1];
+			}
+			if (twoCharElement >=10 && oneCharElement <= 26) {
+				array[index] = array[index] + array[index - 2];
+			}
+		}
+		count = array[strLength];
+		return count;
+
+	}
+
+	public static void main(String[] args) {
+//		String str = "12";
+//		String str = "226";
+//		String str = "0";
+//		String str = "06";
+		String str = "27";
+		System.out.println(decodeWays(str));
+	}
+}
