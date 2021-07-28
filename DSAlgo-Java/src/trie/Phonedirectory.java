08a404740613feb72fe9d584b847e29160854178
NehaChopra <nchopra456@gmail.com>
2021-07-28 12:12:31 +0530

    Day2

--- DSAlgo-Java/src/trie/Phonedirectory.java
+++ DSAlgo-Java/src/trie/Phonedirectory.java
@@ -1,107 +1,128 @@
-//https://practice.geeksforgeeks.org/problems/phone-directory/0
 package trie;
 
-import java.util.Scanner;
+/**
+ * https://www.geeksforgeeks.org/implement-a-phone-directory/
+ * 
+ * Implement a Phone Directory Difficulty Level : Hard Last Updated : 03 Jan,
+ * 2020 Given a list of contacts which exist in a phone directory. The task is
+ * to implement search query for the phone directory. The search query on a
+ * string ‘str’ displays all the contacts which prefix as ‘str’. One special
+ * property of the search function is that, when a user searches for a contact
+ * from the contact list then suggestions (Contacts with prefix as the string
+ * entered so for) are shown after user enters each character.
+ * 
+ * Note : Contacts in the list consist of only lower case alphabets.
+ * 
+ * Example:
+ * 
+ * Input : contacts [] = {“gforgeeks” , “geeksquiz” } Query String = “gekk”
+ * 
+ * Output : Suggestions based on "g" are geeksquiz gforgeeks
+ * 
+ * Suggestions based on "ge" are geeksquiz
+ * 
+ * No Results Found for "gek"
+ * 
+ * No Results Found for "gekk"
+ *
+ */
+class TrieNodeL {
+	boolean isEndOfWord;
+	TrieNodeL children[] = new TrieNodeL[26];
 
-class TrieNode{
-	boolean endOfWord;
-	TrieNode[] children = new TrieNode[26];
-	
-	TrieNode() {
-		this.endOfWord = false;
-		for(int index=0; index<26; index++) {
-			children[index] = null;
+	TrieNodeL() {
+		this.isEndOfWord = false;
+		for (int index = 0; index < 26; index++) {
+			this.children[index] = null;
 		}
 	}
 }
 
-class Trie{
-	TrieNode root;
-	
+class TrieL {
+	TrieNodeL root;
+
 	public void insert(String key) {
-		TrieNode crawler = root;
-		for(int index=0; index<key.length(); index++) {
-			int elementIndex = key.charAt(index) - 'a';
-			if(crawler.children[elementIndex] == null) {
-				crawler.children[elementIndex] = new TrieNode();
+		TrieNodeL node_crawler = root;
+		for (int index = 0; index < key.length(); index++) {
+			int element_index = key.charAt(index) - 'a';
+			if (node_crawler.children[element_index] == null) {
+				node_crawler.children[element_index] = new TrieNodeL();
 			}
-			crawler = crawler.children[elementIndex]; 
+			node_crawler = node_crawler.children[element_index];
 		}
-		crawler.endOfWord = true;
+		node_crawler.isEndOfWord = true;
 	}
-	
+
 	public boolean search(String key) {
-		TrieNode crawler = root;
-		for(int index=0; index<key.length(); index++) {
-			int elementIndex = key.charAt(index) - 'a';
-			if(crawler.children[elementIndex] == null) {
+		TrieNodeL node_crawler = root;
+		for (int index = 0; index < key.length(); index++) {
+			int element_index = key.charAt(index) - 'a';
+			if (node_crawler.children[element_index] == null) {
 				return false;
 			}
-			crawler = crawler.children[elementIndex]; 
+			node_crawler = node_crawler.children[element_index];
 		}
-		return (crawler!= null && crawler.endOfWord);
+		return ((node_crawler != null) && (node_crawler.isEndOfWord));
 	}
-	
-	public void autoSuggest(String key) {
-		TrieNode crawler = root;
-		for(int index=0; index<key.length(); index++) {
-			int elementIndex = key.charAt(index) - 'a';
-			if(crawler.children[elementIndex] != null) {
-				crawler = crawler.children[elementIndex]; 
-			}else {
-				System.out.print("0");
+
+	public void printAutoSuggestList(String key) {
+		TrieNodeL node_crawler = root;
+		for (int index = 0; index < key.length(); index++) {
+			int element_index = key.charAt(index) - 'a';
+			if (node_crawler.children[element_index] != null) {
+				node_crawler = node_crawler.children[element_index];
+			} else {
 				return;
 			}
 		}
-		if(crawler!=null && lastNode(crawler)) {
-			System.out.print(key + " ");
-		}else if(!lastNode(crawler)) {
-			printSuggestedWords(crawler, key);
+		if (node_crawler.isEndOfWord && lastNode(node_crawler)) {
+			System.out.println(key);
+		} else if (node_crawler != null && !lastNode(node_crawler)) {
+			suggestRecords(node_crawler, key);
 		}
 	}
-	
-	public boolean lastNode(TrieNode root) {
-		for(int index=0; index<26; index++) {
-			if(root.children[index] != null) {
+
+	public static void suggestRecords(TrieNodeL root, String key) {
+		if (root.isEndOfWord) {
+			System.out.println(key);
+		}
+		if (lastNode(root)) {
+			return;
+		}
+		for (int index = 0; index < 26; index++) {
+			if (root.children[index] != null) {
+				suggestRecords(root.children[index], key + String.valueOf((char) ('a' + index)));
+			}
+		}
+	}
+
+	public static boolean lastNode(TrieNodeL root) {
+		for (int index = 0; index < 26; index++) {
+			if (root.children[index] != null) {
 				return false;
 			}
 		}
 		return true;
 	}
-	public void printSuggestedWords(TrieNode root, String key) {
-		if(root.endOfWord) {
-				System.out.print(key + " ");
-		}
-		if(lastNode(root)) {
-			return;
-		}
-		for(int index=0; index<26; index++) {
-			if(root.children[index] != null) {
-				printSuggestedWords(root.children[index], key + String.valueOf((char)(index + 'a')));
-			}
-		}
-	}
 }
-class Phonedirectory {
-	public static void main(String []args) {
-		Scanner input = new Scanner(System.in);
-		int testCases = input.nextInt();
-		for(int index=0; index<testCases; index++) {
-			TrieNode root = new TrieNode();
-			Trie obj = new Trie();
-			obj.root = root;
-			int arrayLength = input.nextInt();
-			for(int jindex=0; jindex<arrayLength; jindex++) {
-				obj.insert(input.next());
-			}
-			
-		 	String inputString = input.next();
-		 	StringBuffer str = new StringBuffer();
-		 	for(int mindex=0; mindex<inputString.length(); mindex++) {
-		 		str.append(inputString.charAt(mindex));
-		 		obj.autoSuggest(str.toString());
-		 		System.out.println();
-		 	}
-		}
+
+public class Phonedirectory {
+	public static void main(String[] args) {
+		TrieNodeL root = new TrieNodeL();
+		TrieL obj = new TrieL();
+		obj.root = root;
+		obj.insert("gforgeeks");
+		obj.insert("geeksquiz");
+
+		obj.printAutoSuggestList("g");
+
+		System.out.println("==========================================================");
+		obj.printAutoSuggestList("ge");
+		//
+		// System.out.println("==========================================================");
+		obj.printAutoSuggestList("gek");
+
+		System.out.println("==========================================================");
+		obj.printAutoSuggestList("gekk");
 	}
 }
