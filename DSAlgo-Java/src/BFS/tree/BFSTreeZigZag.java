534dceab978783b3a3d786d3f5d0ab11ec81a821
NehaChopra <nchopra456@gmail.com>
2021-12-27 18:03:58 +0530

    day 2

--- DSAlgo-Java/src/BFS/tree/BFSTreeZigZag.java
+++ DSAlgo-Java/src/BFS/tree/BFSTreeZigZag.java
@@ -1,17 +1,16 @@
-// https://www.geeksforgeeks.org/zigzag-tree-traversal/
-// https://leetcode.com/problems/binary-tree-level-order-traversal/
-
+//https://www.geeksforgeeks.org/level-order-tree-traversal/
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
 
 package BFS.tree;
 
-public class BFSTree {
+public class BFSTreeZigZag {
 
-	Node root;
+	private Node root;
 
 	static class Node {
-		int data;
-		Node leftChild;
-		Node rightChild;
+		private int data;
+		private Node leftChild;
+		private Node rightChild;
 
 		public Node(int data) {
 			this.data = data;
@@ -23,19 +22,22 @@ public class BFSTree {
 	public static void BFS(Node root) {
 		int height = getHeight(root);
 		for (int index = 1; index <= height; index++) {
-			printLevelOrderTraversal(root, index);
+			printLevelOrderTraversal(root, index, index);
 		}
 	}
 
-	public static void printLevelOrderTraversal(Node root, int level) {
+	public static void printLevelOrderTraversal(Node root, int level, int height) {
 		if (root == null) {
 			return;
 		}
 		if (level == 1) {
 			System.out.println(root.data);
-		} else {
-			printLevelOrderTraversal(root.leftChild, level - 1);
-			printLevelOrderTraversal(root.rightChild, level - 1);
+		} else if(height%2 == 0){//even height
+			printLevelOrderTraversal(root.rightChild, level - 1, height);
+			printLevelOrderTraversal(root.leftChild, level - 1, height);
+		}else if(height%2 == 1){//even height
+			printLevelOrderTraversal(root.leftChild, level - 1, height);
+			printLevelOrderTraversal(root.rightChild, level - 1, height);
 		}
 	}
 
@@ -51,7 +53,7 @@ public class BFSTree {
 	}
 
 	public static void main(String[] args) {
-		BFSTree tree = new BFSTree();
+		BFSTreeZigZag tree = new BFSTreeZigZag();
 		tree.root = new Node(1);
 		tree.root.leftChild = new Node(2);
 		tree.root.rightChild = new Node(3);
@@ -63,11 +65,10 @@ public class BFSTree {
 }
 
 
-
-
 /**
+ *********************************************************************Leetcode****************************************************************************
  * 
- **************************************************Leetcode***********************************************************
+ * 
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -82,8 +83,7 @@ public class BFSTree {
  *     }
  * }
  */
-
-//class Solution {    
+//class Solution {
 //    public static int getHeight(TreeNode root){
 //        if(root == null){
 //            return 0;
@@ -94,24 +94,28 @@ public class BFSTree {
 //        return (Math.max(leftChildHeight, rightChildHeight) + 1);
 //    }
 //    
-//    public static void levelOrderTraversal(TreeNode root, int level, List<Integer> traversalList){
+//    public static void levelOrderTraversal(TreeNode root, int level,int height, List<Integer> traversalList){
 //        if(root == null){
 //            return;
 //        }
 //        if(level == 1){
 //            traversalList.add(root.val);
-//        }else{
-//            levelOrderTraversal(root.left, level-1, traversalList);
-//            levelOrderTraversal(root.right, level-1, traversalList);
+//        }else if(height%2 == 0){//even height
+//            levelOrderTraversal(root.right, level-1,height, traversalList);
+//            levelOrderTraversal(root.left, level-1,height, traversalList);
+//        }else if(height%2 == 1){//odd height
+//            levelOrderTraversal(root.left, level-1,height, traversalList);
+//            levelOrderTraversal(root.right, level-1,height, traversalList);
 //        }
 //    }
 //    
-//    public List<List<Integer>> levelOrder(TreeNode root) {
+//    
+//    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 //        List<List<Integer>> finalList = new ArrayList<List<Integer>>();
 //        int height = getHeight(root);
 //        for(int level = 1; level <= height; level++){
 //            List<Integer> traversalList = new ArrayList<Integer>();
-//            levelOrderTraversal(root, level, traversalList);
+//            levelOrderTraversal(root, level,level, traversalList);
 //            finalList.add(traversalList);
 //        }
 //        return finalList;
